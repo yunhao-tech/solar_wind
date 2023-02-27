f0fde12cb914fa5fa992b0a3768616ac32e76897
yunhao-tech <yunhao.chen.louis@gmail.com>
2023-02-27 09:36:57 +0000

    version 2

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -2,44 +2,35 @@ from sklearn.base import BaseEstimator
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+import numpy as np
 
 
-def compute_rolling_std(X_df, feature, time_window, center=False):
-    """
-    For a given dataframe, compute the standard deviation over
-    a defined period of time (time_window) of a defined feature
-
-    Parameters
-    ----------
-    X : dataframe
-    feature : str
-        feature in the dataframe we wish to compute the rolling std from
-    time_window : str
-        string that defines the length of the time window passed to `rolling`
-    center : bool
-        boolean to indicate if the point of the dataframe considered is
-        center or end of the window
-    """
-    name = "_".join([feature, time_window, "std"])
-    X_df[name] = X_df[feature].rolling(time_window, center=center).std()
-    X_df[name] = X_df[name].ffill().bfill()
-    X_df[name] = X_df[name].astype(X_df[feature].dtype)
+def compute_rolling_std(X_df, features, time_window, center=False):
+    for feature in features:
+        name = "_".join([feature, time_window, "std"])
+        X_df[name] = X_df[feature].rolling(time_window, center=center).std()
+        X_df[name] = X_df[name].ffill().bfill()
+        X_df[name] = X_df[name].astype(X_df[feature].dtype)
     return X_df
 
+def add_features(X_df):
+    Alfven_Mach_number = X_df['V'] * 1e12 * np.sqrt(X_df['Np'] * 1.7e-27 * 1e6) * np.sqrt(4e-7*np.pi) / X_df['B']
+    raw_pressure = X_df['V']**2 * X_df['Np'] * 1.7e-27 * 1e12 * 1e9
+    X_df['AMach_number'] = Alfven_Mach_number
+    X_df['raw_pressure'] = raw_pressure
+    return X_df
 
 class FeatureExtractor(BaseEstimator):
     def fit(self, X, y):
         return self
 
     def transform(self, X):
-        return compute_rolling_std(X, "Beta", "2h")
-
+        X = add_features(X)
+        return compute_rolling_std(X, ["Beta", "Vth", "B", "Bx", "Bz"], "2h")
 
 def get_estimator():
 
     feature_extractor = FeatureExtractor()
-
     classifier = LogisticRegression(max_iter=1000)
-
     pipe = make_pipeline(feature_extractor, StandardScaler(), classifier)
     return pipe
